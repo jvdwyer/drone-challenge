6f8f80f4ddd610c99a830c696bd588d04f6ccbb5
jvdwyer <John.vaughan.dwyer@gmail.com>
2024-03-14 10:38:58 -0500

    Added read csv to routing_service.py and fixed an issue in calling validate_token request.

--- src/routing_service.py
+++ src/routing_service.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, jsonify
 import requests
 import json
+import csv
 
 app = Flask(__name__)
 
 CONFIG_FILE_PATH = 'config/application_settings.json'
+CSV_FILE_PATH = 'test.csv'
+
+# Reads CSV
+def read_csv():
+    data = []
+    with open(CSV_FILE_PATH, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 # Returns service port
 def get_service_port(service_name='routing_service'):
@@ -19,16 +30,17 @@ AUTH_SERVICE_URL = f'http://localhost:{get_service_port('authentication_service'
 # Route returns next destination
 @app.route('/get_destination', methods=['GET'])
 def get_destination():
-    token = request.headers.authentication
+    token = request.authorization.token
 
     if not token:
         return jsonify({'error': 'Token is missing'}), 401
     
-    auth_response = requests.post(AUTH_SERVICE_URL, headers = {'Authorization': token})
+    auth_response = requests.post(AUTH_SERVICE_URL + '/validate_token', headers = {'Authorization': token})
 
     if auth_response.status_code == 200:
         ## TODO serve next destination
-        return 200
+        data = read_csv()
+        return jsonify(data), 200
     else:
         return jsonify({auth_response.request.body}), auth_response.status_code
     
